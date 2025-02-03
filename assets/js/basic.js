5a599ec9f7490d9b10f873487cf5d2d9c56b5a1b
sunita0987 <sunita0987546@gmail.com>
2025-02-03 14:03:44 +0530

    add section-4

--- assets/js/basic.js
+++ assets/js/basic.js
@@ -2,12 +2,6 @@ function toggleMenu() {
     const nav = document.querySelector(".nav-links");
     nav.classList.toggle("active");
 }
-// document.querySelectorAll(".accordion-header").forEach((header) => {
-//     header.addEventListener("click", () => {
-//         const item = header.parentElement;
-//         item.classList.toggle("active");
-//     });
-// });
 document.querySelectorAll(".faq-question").forEach(button => {
     button.addEventListener("click", () => {
         const faqItem = button.parentElement;
